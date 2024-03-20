c1812400972de3d40a2a38fee5a2e0195a7f8378
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-03-20 13:31:14 +0200

    add side bar toggle

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Aside() {
+    const [isOpen, setIsOpen ] = useState(false) 
     
 
     return (
-        <>
-            <Link to="/">
-                <span className='icon'>
+        <div className={'list ' + (isOpen ? "open" : "")}>
+            <Link to="/" className='toggle'>
+                <span className={'icon'} onClick={() => setIsOpen(!isOpen)}>
                     <svg>
                         <path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z" />
                     </svg>
@@ -20,6 +21,7 @@ export default function Aside() {
                         <path d="m12 4.44 7 6.09V20h-4v-6H9v6H5v-9.47l7-6.09m0-1.32-8 6.96V21h6v-6h4v6h6V10.08l-8-6.96z"></path>
                     </svg>
                 </span>
+                <span class="title">Home</span>
             </Link>
             <Link to="/">
                 <span className='icon'>
@@ -27,6 +29,7 @@ export default function Aside() {
                         <path d="M10 14.65v-5.3L15 12l-5 2.65zm7.77-4.33-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zm-.23 5.86-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46c-.99-.41-1.65-1.35-1.7-2.41-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L15.5 9.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56z"></path>
                     </svg>
                 </span>
+                <span class="title">Shorts</span>
             </Link>
             <span className='separator'></span>
             <Link to="/">
@@ -37,6 +40,7 @@ export default function Aside() {
                         </g>
                     </svg>
                 </span>
+                <span class="title">History</span>
             </Link>
             <Link to="/">
                 <span className='icon'>
@@ -44,8 +48,9 @@ export default function Aside() {
                         <path d="M14.97 16.95 10 13.87V7h2v5.76l4.03 2.49-1.06 1.7zM12 3c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2z"></path>
                     </svg>
                 </span>
+                <span class="title">Watch Later</span>
             </Link>
           
-        </>
+        </div>
     )
 }
--- src/routes/Root.js
+++ src/routes/Root.js
@@ -24,7 +24,6 @@ export default function Root() {
             <section className='content-list'>
                 <Outlet/>
             </section>
-            
         </main>
         </>
     )
