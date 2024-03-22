c04753c320f73988e9c93337af3adba94d974fab
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-03-22 14:53:39 +0200

    extract video item component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,4 +3,20 @@ import {Link} from "react-router-dom";
 import { timeAgo } from "../helpers/date";
 
 
-
+export default function VideoItem({video, index}) {
+    return (
+        <Link to={"/v/" + video?.id} className="item" key ={index}>
+            <div className="img-holder">
+                <img src={video?.thumbnails} alt={video?.title} />
+            </div>
+            <div className="details">
+                <span className="title">{video?.title}</span>
+                <div className="tips">
+                    <span className="channel">{video?.channelTitle}</span>
+                    <span className="date">{timeAgo(video?.publishedAt)}</span>
+                    <span className="duration">{video?.duration}</span>
+                </div>
+            </div>
+        </Link>
+    )
+}
