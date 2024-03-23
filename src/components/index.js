2cbd854841ab4c31f9dde8ea1f3a6cc75afc7af0
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-03-23 15:38:18 +0200

    extract video details component from video route

--- src/components/index.js
+++ src/components/index.js
@@ -2,13 +2,10 @@ export {default as Aside} from "./Aside";
 export {default as Categories} from "./Categories";
 export {default as NavBar} from "./NavBar";
 export {default as ErrorPage} from "./ErrorPage";
-
-
 export {default as SearchBar} from "./SearchBar";
-export {default as VideoDetails} from "./Video/VideoDetails";
-export {default as VideoList} from "./Video/VideoList";
+
+export {default as VideoDetails} from "./VideoDetails";
 export {default as VideoItem} from "./VideoItem";
-export {default as Video} from "./Video";
 export {default as Root} from "../routes/Root";
 export {default as Search} from "./Search";
 export {default as NotFound} from "./NotFound";
--- src/routes/Video.js
+++ src/routes/Video.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useLocation} from "react-router-dom";
+import { VideoDetails } from "../components";
 import { getVideo } from "../helpers/data";
 
 export async function loader({params}) {
@@ -19,23 +20,7 @@ export default function Video() {
     }else{
         video = getVideo(id)
     }
-
-
-    const videoSrc = "https://www.youtube.com/embed/" + id;
-
-    // console.log(video.snippet);
     
 
-    return (
-        <>
-            <iframe width="100%" height="500px" src={videoSrc+ "?autoplay=1&enable_js=1"} title="Video Player" autoplay={1}
-            allow="accelerometer; autoplay *; clipboard-write; encrypted-media; gyroscope; picture-in-picture;fullscreen"
-            ></iframe>
-            <h1>{video?.title}</h1>
-            <a href={"https://www.youtube.com/channel/"+video?.channelTitle}>{video?.channelTitle}</a>
-            <div className="description">
-                {video?.description}
-            </div>
-        </>
-    )
+    return <VideoDetails video={video} id={id}/>
 }
