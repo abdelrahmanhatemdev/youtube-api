cf8faa520cb4f94f3656385684d1d965b054eaa0
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-03-26 08:19:15 +0200

    hide old lives from categories component

--- src/components/Category/VideoItem.js
+++ src/components/Category/VideoItem.js
@@ -5,25 +5,34 @@ import { dateFormat, durationFormat, viewsFormat } from "../../helpers/formats";
 
 export default function VideoItem({video, index, videos}) {
 
+    const oldLiveImg = "/hqdefault_live.jpg";
+    let videoItem;
+
+    if (video.thumbnails.includes(oldLiveImg)) {
+        videoItem = ""
+    }else{
+        videoItem =  <Link to={"/v/" + video.id} className="item" key ={index} state={{video: video, videos:videos}}>
+        <div className="img-holder">
+            <img src={video.thumbnails} alt={video.title} />
+            <span className="duration">{durationFormat(video.duration)}</span>
+        </div>
+        <div className="details">
+            <span className="title">{video.title}</span>
+            <div className="tips">
+                <span className="channel">{video.channelTitle}</span>
+                <span className="date">
+                    {viewsFormat(video.viewCount)} • {dateFormat(video.publishedAt)}</span>
+                
+            </div>
+        </div>
+    </Link>
+    }
+
+
+
    
 
   
 
-    return (
-        <Link to={"/v/" + video?.id} className="item" key ={index} state={{video: video, videos:videos}}>
-            <div className="img-holder">
-                <img src={video?.thumbnails} alt={video?.title} />
-                <span className="duration">{durationFormat(video?.duration)}</span>
-            </div>
-            <div className="details">
-                <span className="title">{video?.title}</span>
-                <div className="tips">
-                    <span className="channel">{video?.channelTitle}</span>
-                    <span className="date">
-                        {viewsFormat(video?.viewCount)} • {dateFormat(video?.publishedAt)}</span>
-                    
-                </div>
-            </div>
-        </Link>
-    )
+    return videoItem
 }
