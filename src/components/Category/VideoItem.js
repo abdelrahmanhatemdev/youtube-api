8e9ef3a3584b07c045614cdaf592fa935d84cbb8
Hatem Hekaia <abdelrahmanhatem.dev@gmail.com>
2024-03-26 10:32:38 +0200

    add related videos component styles

--- src/components/Category/VideoItem.js
+++ src/components/Category/VideoItem.js
@@ -1,8 +1,6 @@
 
 import {Link} from "react-router-dom";
 import { dateFormat, durationFormat, numberFormat } from "../../helpers/formats";
-import "./videoItem.css";
-
 
 export default function VideoItem({video, index, videos}) {
 
--- src/components/Video/RelatedVideos.js
+++ src/components/Video/RelatedVideos.js
@@ -1,23 +1,14 @@
 import VideoItem from "../Category/VideoItem";
+import "./relatedVideos.css";
 
 export default function RelatedVideos({videos}) {
-
     let content;
 
-
     if (videos) {
-        
         content= videos.map((video, index) => {
-
             return  video && <VideoItem video={video} index={index}/>;
-    
         })
     }
 
-    return (
-        <>
-        {content}
-            
-        </>
-    )
+    return content;
 }
