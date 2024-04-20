7487e8c7216348e592adbbcd4b912a1123de0f9a
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-04-20 21:59:24 +0200

    add video and videos in short route

--- src/components/Short/index.js
+++ src/components/Short/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import {useLoaderData } from "react-router-dom";
-import { getIntialData } from "../../data/homeData";
+import {useLoaderData, useLocation } from "react-router-dom";
+import { getIntialData} from "../../data/homeData";
 import { isRequestLimit } from "../../data/apiData";
 import { isShort } from "../../helpers/check";
 import ShortDetails from "./ShortDetails";
 
-export async function loader() {
-    const data = await getIntialData();
-    isRequestLimit(data);
-    return {data}
+export async function loader({params}) {
+    const shortId = params.short;
+    console.log(params);
+    const shorts = await getIntialData();
+    isRequestLimit(shorts);
+    return {shortId, shorts}
 }
 
 export default function Short(props) {
-    const {data} = useLoaderData();
+    let video, videos;
+    let {shortId, shorts} = useLoaderData();
+    const{state} = useLocation();
+    if (state) {
+        video = state.video;
+        videos = state.videos;
+    }else{
+        shorts = shorts.filter( v => isShort(v));
+        video = shorts.filter(v => v.id === shortId);
+        videos = shorts;
+    }
     let content;
 
-    if (data) {
-        const shorts = data.filter( v => isShort(v));
-        if (shorts) {
-            // content =<ShortDetails video={shorts[0]} videos={shorts}/>
-        }
-        
-        console.log("shorts", shorts);
+    console.log(video);
+
+    if (video) {
+        content =<ShortDetails video={video} videos={videos}/>
     }
-    
 
     return (
         <section className='shorts-page'>
